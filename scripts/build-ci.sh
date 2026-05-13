9c91b3165bf34b1a8102599b340ac8dd60684bbb
Jonah Nestrick <undecidedapollo@gmail.com>
2026-05-13 18:36:38 -0400

    simplify ci

--- scripts/build-ci.sh
+++ scripts/build-ci.sh
@@ -7,12 +7,10 @@ ZOLA_VERSION="0.22.1"
 ZOLA_TARBALL="zola-v${ZOLA_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
 ZOLA_URL="https://github.com/getzola/zola/releases/download/v${ZOLA_VERSION}/${ZOLA_TARBALL}"
 
-git submodule update --init --recursive
+# git submodule update --init --recursive
 
 if ! [ -x ./zola ]; then
   curl -sSL "$ZOLA_URL" | tar xz
 fi
-
-npm ci
 
 ./zola build
